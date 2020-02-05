6ae85c3b2880ec81af2d92b1fa2580bb088fd30c
lubien <lubien1996@gmail.com>
2020-02-04 23:05:03 -0300

    Initial Commit

---
+++ app/Controllers/Http/GameController.js
@@ -0,0 +1,13 @@
+'use strict'
+
+class GameController {
+  render ({ request, view }) {
+    const guessedNumber = Number(request.input('number'))
+    const randomNumber = Math.floor(Math.random() * 20) + 1
+
+    /** rendering view */
+    return view.render('game', { guessedNumber, randomNumber })
+  }
+}
+
+module.exports = GameController
